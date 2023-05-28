552f0a639e2f6bc9d01d811f38c35dd09ce8856e
sanchit-9068 <sanchitawasthi2525@gmail.com>
2023-05-28 19:29:14 +0530

    first commit

---
+++ CPU1_RAM/adc_ex1_soc_software.d
@@ -0,0 +1,264 @@
+# FIXED
+
+adc_ex1_soc_software.obj: ../adc_ex1_soc_software.c
+adc_ex1_soc_software.obj: C:/Users/Sanchit\ Awasthi/workspace_v12/adc_ex1_soc_software/device/driverlib.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_memmap.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/adc.h
+adc_ex1_soc_software.obj: C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/stdbool.h
+adc_ex1_soc_software.obj: C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/_ti_config.h
+adc_ex1_soc_software.obj: C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/linkage.h
+adc_ex1_soc_software.obj: C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/stdint.h
+adc_ex1_soc_software.obj: C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/_stdint40.h
+adc_ex1_soc_software.obj: C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/stdint.h
+adc_ex1_soc_software.obj: C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/cdefs.h
+adc_ex1_soc_software.obj: C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/_types.h
+adc_ex1_soc_software.obj: C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/machine/_types.h
+adc_ex1_soc_software.obj: C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/machine/_stdint.h
+adc_ex1_soc_software.obj: C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/_stdint.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_adc.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_sysctl.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_types.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/cpu.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/debug.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/asysctl.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_asysctl.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/can.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_can.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/sysctl.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_nmi.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/interrupt.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_ints.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_pie.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/cla.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_cla.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/clb.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_clb.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/cmpss.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_cmpss.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/cputimer.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_cputimer.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/dac.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_dac.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/dcsm.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_dcsm.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/dma.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_dma.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/ecap.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_ecap.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/emif.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_emif.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_memcfg.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/epwm.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_epwm.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/eqep.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_eqep.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/flash.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_flash.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/gpio.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_gpio.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_xint.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/xbar.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_clbxbar.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_epwmxbar.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_inputxbar.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_outputxbar.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_xbar.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/hrpwm.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_hrpwm.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/hrpwm.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/i2c.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_i2c.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/hw_reg_inclusive_terminology.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/ipc.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_ipc.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/mcbsp.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_mcbsp.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/memcfg.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/pin_map.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/pin_map_legacy.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/sci.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_sci.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/sdfm.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_sdfm.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/spi.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_spi.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/upp.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_upp.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/version.h
+adc_ex1_soc_software.obj: C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/driver_inclusive_terminology_mapping.h
+adc_ex1_soc_software.obj: C:/Users/Sanchit\ Awasthi/workspace_v12/adc_ex1_soc_software/device/device.h
+
+../adc_ex1_soc_software.c:
+
+C:/Users/Sanchit\ Awasthi/workspace_v12/adc_ex1_soc_software/device/driverlib.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_memmap.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/adc.h:
+
+C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/stdbool.h:
+
+C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/_ti_config.h:
+
+C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/linkage.h:
+
+C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/stdint.h:
+
+C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/_stdint40.h:
+
+C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/_types.h:
+
+C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/machine/_types.h:
+
+C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1230/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/_stdint.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_adc.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_sysctl.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_types.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/cpu.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/debug.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/asysctl.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_asysctl.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/can.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_can.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/sysctl.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_nmi.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/interrupt.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_ints.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_pie.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/cla.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_cla.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/clb.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_clb.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/cmpss.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_cmpss.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/cputimer.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_cputimer.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/dac.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_dac.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/dcsm.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_dcsm.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/dma.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_dma.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/ecap.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_ecap.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/emif.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_emif.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_memcfg.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/epwm.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_epwm.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/eqep.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_eqep.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/flash.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_flash.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/gpio.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_gpio.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_xint.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/xbar.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_clbxbar.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_epwmxbar.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_inputxbar.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_outputxbar.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_xbar.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/hrpwm.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_hrpwm.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/hrpwm.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/i2c.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_i2c.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/hw_reg_inclusive_terminology.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/ipc.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_ipc.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/mcbsp.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_mcbsp.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/memcfg.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/pin_map.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/pin_map_legacy.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/sci.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_sci.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/sdfm.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_sdfm.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/spi.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_spi.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/upp.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/inc/hw_upp.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/version.h:
+
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/driver_inclusive_terminology_mapping.h:
+
+C:/Users/Sanchit\ Awasthi/workspace_v12/adc_ex1_soc_software/device/device.h:
+
---
+++ CPU1_RAM/subdir_vars.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../2837xD_RAM_lnk_cpu1.cmd 
+
+LIB_SRCS += \
+C:/ti/c2000/C2000Ware_4_03_00_00/libraries/calibration/hrpwm/f2837xd/lib/SFO_v8_fpu_lib_build_c28_driverlib.lib \
+C:/ti/c2000/C2000Ware_4_03_00_00/driverlib/f2837xd/driverlib/ccs/Debug/driverlib.lib 
+
+C_SRCS += \
+../adc_ex1_soc_software.c 
+
+C_DEPS += \
+./adc_ex1_soc_software.d 
+
+OBJS += \
+./adc_ex1_soc_software.obj 
+
+OBJS__QUOTED += \
+"adc_ex1_soc_software.obj" 
+
+C_DEPS__QUOTED += \
+"adc_ex1_soc_software.d" 
+
+C_SRCS__QUOTED += \
+"../adc_ex1_soc_software.c" 
+
+
